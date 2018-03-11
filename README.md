64467d5812a522fb3439d6d1014879b85d7e6580
JP Dillingham <jpdillingham@gmail.com>
2018-03-10 18:17:49 -0600

    Cleanup, condensed code to single file, added README

---
+++ README.md
@@ -0,0 +1,5 @@
+# aws-lambda-express
+
+This is a bare-bones, bare minimum implementation of an AWS Lambda script using [Express.js](https://expressjs.com/).
+
+I previously tried the official AWS Labs example and was unable to get it to work, so I made this.
--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const awsServerlessExpress = require('aws-serverless-express');  
-const app = require('./app');  
+const path = require('path');  
+const express = require('express');  
+const bodyParser = require('body-parser');  
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());  
+app.use(bodyParser.json());  
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/hello', function(req, res) {  
+    res.status(200);
+    res.json('Hello world');
+});
+
+app.listen(3000, () => console.log('Listening on port 3000.')); // ignored by lambda
+
 const server = awsServerlessExpress.createServer(app);
 
 exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context);  
